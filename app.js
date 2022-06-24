8b76acac820aca036818a8af42074ddef450e052
Andres Alcantara <aalcantaratorres@outlook.com>
2022-06-23 21:58:05 -0500

    cambie la funcion por un addEventListener y añadi un ID al form

--- app.js
+++ app.js
@@ -1,28 +1,23 @@
 var Username = document.getElementById('username');
 var Password = document.getElementById('password');
 var ErrorMessage = document.getElementById('errorMessage');
+var LoginForm = document.getElementById('loginForm');
 
-function sendFormulary(){
-    console.log("Enviando Formulario...");
-    
-
+LoginForm.addEventListener('submit', function(evt){
+    evt.preventDefault();
     var ErrorMessages = [];
 
     if (Username.value === null || Username.value === '') {
         ErrorMessages.push('Ingresa tu Nombre de Usuario');
         ErrorMessage.hidden = false;
     }
-
     if (Password.value === null || Password.value === '') {
         ErrorMessages.push('Ingresa tu Contraseña');
         ErrorMessage.hidden = false;
     }
-
     if (ErrorMessages.length === 0) {
-        ErrorMessage.hidden = true;       
+        ErrorMessage.hidden = true;
     }
 
     ErrorMessage.innerHTML = ErrorMessages.join(' e ');
-
-    return false;
-}
+});
